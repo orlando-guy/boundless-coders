7c019e882a2830aad32fc38e1cfa79e18083f50d
Orlando <guichardorlando@gmail.com>
2023-12-18 14:50:14 +0100

    fix(challenge): remove pagination when T.I lte PPI

--- app/ui/pagination.tsx
+++ app/ui/pagination.tsx
@@ -5,7 +5,8 @@ import { unstable_Pagination as CdsPagination } from "@carbon/react"
 import { usePathname, useSearchParams, useRouter } from 'next/navigation'
 
 export default function Pagination({ totalItems }: Readonly<{ totalItems: number }>) {
-    const [currentPageSize, setCurrentPageSize] = useState(10)
+    const ITEM_PER_PAGE = 10
+    const [currentPageSize, setCurrentPageSize] = useState(ITEM_PER_PAGE)
     const searchParams = useSearchParams()
     const pathname = usePathname()
     const { replace } = useRouter()
@@ -20,12 +21,16 @@ export default function Pagination({ totalItems }: Readonly<{ totalItems: number
         replace(`${pathname}?${params.toString()}`)
     }
 
+    if (totalItems <= ITEM_PER_PAGE) {
+        return <></>
+    }
+
     return (
         <CdsPagination
             backwardText="Previous page"
             forwardText="Next page"
             itemsPerPageText="Items per page:"
-            onChange={({ page, pageSize }: {page: number, pageSize: number}) => {
+            onChange={({ page, pageSize }: { page: number, pageSize: number }) => {
                 if (pageSize !== currentPageSize) {
                     setCurrentPageSize(pageSize)
                 }
@@ -33,7 +38,7 @@ export default function Pagination({ totalItems }: Readonly<{ totalItems: number
             }}
             page={1}
             pageSize={currentPageSize}
-            pageSizes={[10]}
+            pageSizes={[ITEM_PER_PAGE]}
             size="md"
             totalItems={totalItems}
         />
