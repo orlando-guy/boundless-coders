2bce7788e9afce03c494d51320a25e101cce889c
Orlando <guichardorlando@gmail.com>
2024-01-23 09:44:14 +0100

    chore: update a part of style

--- app/(auth)/dashboard/(user)/in/page.tsx
+++ app/(auth)/dashboard/(user)/in/page.tsx
@@ -1,14 +1,15 @@
 import { filteredChallenges } from "@/app/lib/data";
+import { ChallengeTable } from "@/app/ui/Table";
 import TrendChallengeWidget from "@/app/ui/challenges/trend-challenge-widget";
 
 export default async function ChallengeManagerPage() {
     const suggestedChallenges = await filteredChallenges(1, '')
 
     return (
-        <>
+        <div className="dashboard-responsive">
             <h1>Tableau de bord</h1>
             <br />
             <TrendChallengeWidget challenges={suggestedChallenges} />
-        </>
+        </div>
     )
 }
