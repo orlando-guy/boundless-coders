e28ae062e083f0817c8faad6b14a191a64d77f14
Orlando <guichardorlando@gmail.com>
2024-01-24 00:40:16 +0100

    chore(Footer): add new content

--- app/ui/footer/footer.tsx
+++ app/ui/footer/footer.tsx
@@ -27,7 +27,7 @@ const Footer = () => {
             <div className="w-100 footer__menu-groups">
                 <Grid>
                     {/* Here's come other footer menu groups */}
-                    <Column sm={4}>
+                    <Column sm={4} className="py-2">
                         <div className="flex flex-col">
                             <h5 className="heading-01">Suivre BOUNDLESS CODERS</h5>
                             <ul className="list-unstyle mt-2">
@@ -41,9 +41,42 @@ const Footer = () => {
                             </ul>
                         </div>
                     </Column>
+                    <Column sm={4} className="py-2">
+                        <div className="flex flex-col">
+                            <h5 className="heading-01">Le projet</h5>
+                            <ul className="list-unstyle mt-2">
+                                <li>
+                                    Développer par {' '}
+                                    <CarbonLink
+                                        href="https://my-portfolio-orlando-guy.vercel.app/"
+                                        className="text-white-20"
+                                        target="_blank"
+                                    >Orlando Guichard</CarbonLink>
+                                </li>
+                                <li>
+                                    <CarbonLink
+                                        href="https://www.linkedin.com/in/orlando-guychard-731a15201"
+                                        className="text-white-20"
+                                        target="_blank"
+                                    >
+                                        Mon LinkedIn
+                                    </CarbonLink>
+                                </li>
+                                <li>
+                                    <CarbonLink
+                                        href="https://github.com/orlando-guy"
+                                        className="text-white-20"
+                                        target="_blank"
+                                    >
+                                        Mon Github
+                                    </CarbonLink>
+                                </li>
+                            </ul>
+                        </div>
+                    </Column>
                 </Grid>
             </div>
-            <p className="text-center w-full text-sm mt-2">Copyright © 2023 BOUNDLESS CODERS.</p>
+            <p className="text-center w-full text-sm mt-3">Copyright © {(new Date()).getFullYear()} BOUNDLESS CODERS.</p>
         </footer>
     )
 }
